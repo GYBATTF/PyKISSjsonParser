6f570b46612471dd17692da39f67d66f96382bea
GYBATTF <51380671+GYBATTF@users.noreply.github.com>
2019-08-09 22:04:13 -0500

    Clean up code

--- JSONLoader.py
+++ JSONLoader.py
@@ -1,11 +1,11 @@
 import JSONParser
 from urllib.request import urlopen
 
-def load(s, download):
-    if (download):
-        data = urlopen(s).read()
-        s = data.decode("utf-8")
+def download(url):
+    data = urlopen(url).read()
+    s = data.decode("utf-8")
     
     return JSONParser.parse(s)
 
-load("https://raw.githubusercontent.com/LearnWebCode/json-example/master/pets-data.json", True)
+def load(json)
+    return JSONParser.parse(json)
